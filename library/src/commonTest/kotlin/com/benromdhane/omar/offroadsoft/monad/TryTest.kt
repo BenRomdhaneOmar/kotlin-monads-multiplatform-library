c59e484e7e3597e8b7311ab64ff20a6ad0f66005
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2025-12-27 13:50:20 +0100

    implement try monad to maybe success function

--- library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/TryTest.kt
+++ library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/TryTest.kt
@@ -1,7 +1,9 @@
 package com.benromdhane.omar.offroadsoft.monad
 
 import com.benromdhane.omar.offroadsoft.monad.error.Try
+import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 import kotlin.uuid.ExperimentalUuidApi
@@ -61,12 +63,34 @@ class TryTest {
     }
 
     @Test
-    fun `trying must return success if the provided expression does not throws an exception`() {
+    fun `trying must return success if the provided expression does not throw an exception`() {
         val initialValue = { 10 / 1 }
         val result =
             Try.trying(initialValue)
                 .success()
 
         assertTrue { result }
+    }
+
+    @Test
+    fun `to maybe success must return empty maybe if the initial try was initiated as failure`() {
+        val initialValue = Exception()
+        val result =
+            Try.seed<Int>(initialValue)
+                .toMaybeSuccess()
+                .empty()
+
+        assertTrue { result }
+    }
+
+    @Test
+    fun `to maybe success must return maybe with initial expression if the initial try was initiated as success`() {
+        val initialValue = Random.nextInt()
+        val result =
+            Try.seed(initialValue)
+                .toMaybeSuccess()
+                .orNull()
+
+        assertEquals(initialValue, result)
     }
 }
