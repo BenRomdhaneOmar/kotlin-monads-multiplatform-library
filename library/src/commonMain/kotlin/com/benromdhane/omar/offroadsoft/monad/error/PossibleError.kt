d904da3a26b0e1346f8a3e306028735088a7aea3
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-01 17:35:23 +0100

    create possible error structure and builder from try without expected success and error indicator function

---
+++ library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/error/PossibleError.kt
@@ -0,0 +1,44 @@
+package com.benromdhane.omar.offroadsoft.monad.error
+
+sealed interface PossibleError<ERROR : Any> {
+
+    fun error(): Boolean
+
+    companion object Builder {
+
+        fun of(`try`: Try<Unit>) =
+            `try`.toMaybeFailure()
+                .map { Error.of<Throwable>(it) }
+                .or(Success.of())
+    }
+
+    @ConsistentCopyVisibility
+    private data class Error<ERROR : Any> private constructor(
+        val error: ERROR
+    ) : PossibleError<ERROR> {
+
+        override fun error() = true
+
+        companion object Builder {
+
+            fun <ERROR : Any> of(error: ERROR): PossibleError<ERROR> = Error(error)
+        }
+    }
+
+    private class Success<ERROR : Any> private constructor() : PossibleError<ERROR> {
+
+        override fun error() = false
+        override fun toString() = "Success()"
+        override fun equals(other: Any?) =
+            if (this === other) true
+            else if (other == null || this::class != other::class) false
+            else true
+
+        override fun hashCode() = this::class.hashCode()
+
+        companion object Builder {
+
+            fun <ERROR : Any> of(): PossibleError<ERROR> = Success()
+        }
+    }
+}
---
+++ library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/error/PossibleErrorTest.kt
@@ -0,0 +1,30 @@
+package com.benromdhane.omar.offroadsoft.monad.error
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class PossibleErrorTest {
+
+    @Test
+    fun `error must return false if possible error is created by try that was initiated as success`() {
+        val result =
+            PossibleError.of(Try.seed(Unit))
+                .error()
+
+        assertFalse { result }
+    }
+
+    @Test
+    fun `error must return true if possible error is created by try that was initiated as failure`() {
+        val initialError = Exception(Uuid.random().toString())
+        val result =
+            PossibleError.of(Try.seed(initialError))
+                .error()
+
+        assertTrue { result }
+    }
+}
