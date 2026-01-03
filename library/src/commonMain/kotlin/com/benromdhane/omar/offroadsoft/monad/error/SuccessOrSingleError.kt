045e52b8e8aac87fe0c573f5a185e5ba90f664b4
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-03 12:41:56 +0100

    define success or single error data structure and implement success indicator

---
+++ library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/error/SuccessOrSingleError.kt
@@ -0,0 +1,32 @@
+package com.benromdhane.omar.offroadsoft.monad.error
+
+sealed interface SuccessOrSingleError<SUCCESS : Any, ERROR : Any> {
+
+    fun success(): Boolean
+
+    @ConsistentCopyVisibility
+    data class Success<SUCCESS : Any, ERROR : Any> private constructor(
+        val success: SUCCESS
+    ) : SuccessOrSingleError<SUCCESS, ERROR> {
+
+        override fun success() = true
+
+        companion object Builder {
+
+            fun <SUCCESS : Any, ERROR : Any> of(success: SUCCESS) = Success<SUCCESS, ERROR>(success)
+        }
+    }
+
+    @ConsistentCopyVisibility
+    data class Error<SUCCESS : Any, ERROR : Any> private constructor(
+        val error: ERROR
+    ) : SuccessOrSingleError<SUCCESS, ERROR> {
+
+        override fun success() = false
+
+        companion object Builder {
+
+            fun <SUCCESS : Any, ERROR : Any> of(error: ERROR) = Error<SUCCESS, ERROR>(error)
+        }
+    }
+}
---
+++ library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/error/SuccessOrSingleErrorTest.kt
@@ -0,0 +1,33 @@
+package com.benromdhane.omar.offroadsoft.monad.error
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class SuccessOrSingleErrorTest {
+
+    @Test
+    fun `success must return false if success of single error was initiated as error`() {
+        val result =
+            SuccessOrSingleError
+                .Error
+                .of<String, _>(Exception(Uuid.random().toString()))
+                .success()
+
+        assertFalse { result }
+    }
+
+    @Test
+    fun `success must return true if success of single error was initiated as success`() {
+        val result =
+            SuccessOrSingleError
+                .Success
+                .of<_, Throwable>(Uuid.random().toString())
+                .success()
+
+        assertTrue { result }
+    }
+}
