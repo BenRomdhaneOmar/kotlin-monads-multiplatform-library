33bf59d879239f0b1b4f7b8f31be4000ada623ec
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-06 22:16:35 +0100

    implement to success with condition on all errors provider for success or multiple errors data structure

--- library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/error/SuccessOrMultipleErrors.kt
+++ library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/error/SuccessOrMultipleErrors.kt
@@ -31,6 +31,10 @@ sealed interface SuccessOrMultipleErrors<SUCCESS : Any, ERROR : Any> {
 
     fun toSuccess(alternativeSuccess: SUCCESS): SuccessOrMultipleErrors<SUCCESS, ERROR>
     fun toSuccess(alternativeSuccess: () -> SUCCESS): SuccessOrMultipleErrors<SUCCESS, ERROR>
+    fun toSuccessIfAllErrors(
+        alternativeSuccess: SUCCESS,
+        condition: (ERROR) -> Boolean
+    ): SuccessOrMultipleErrors<SUCCESS, ERROR>
 
     @ConsistentCopyVisibility
     data class Success<SUCCESS : Any, ERROR : Any> private constructor(
@@ -87,6 +91,7 @@ sealed interface SuccessOrMultipleErrors<SUCCESS : Any, ERROR : Any> {
 
         override fun toSuccess(alternativeSuccess: SUCCESS) = this
         override fun toSuccess(alternativeSuccess: () -> SUCCESS) = this
+        override fun toSuccessIfAllErrors(alternativeSuccess: SUCCESS, condition: (ERROR) -> Boolean) = this
 
         companion object Builder {
 
@@ -118,6 +123,14 @@ sealed interface SuccessOrMultipleErrors<SUCCESS : Any, ERROR : Any> {
         override fun filterSuccessNot(alternativeError: () -> ERROR, condition: (SUCCESS) -> Boolean) = this
         override fun toSuccess(alternativeSuccess: SUCCESS) = Success.of<_, ERROR>(alternativeSuccess)
         override fun toSuccess(alternativeSuccess: () -> SUCCESS) = Success.of<_, ERROR>(alternativeSuccess())
+        override fun toSuccessIfAllErrors(
+            alternativeSuccess: SUCCESS,
+            condition: (ERROR) -> Boolean
+        ) =
+            if (this.errors.any { condition(it).not() })
+                this
+            else
+                Success.of(alternativeSuccess)
 
         companion object Builder {
 
