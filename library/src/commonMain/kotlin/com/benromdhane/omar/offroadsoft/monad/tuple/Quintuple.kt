97a9b6b72fa06085e7eb9dc5498e663781fd5984
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-11 12:50:34 +0100

    implement switch first and fourth function for quintuple tuple

--- library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/Quintuple.kt
+++ library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/Quintuple.kt
@@ -82,6 +82,15 @@ data class Quintuple<FIRST_ELEMENT : Any, SECOND_ELEMENT : Any, THIRD_ELEMENT : 
             this.fifthElement
         )
 
+    fun switchFirstAndFourthElements() =
+        Quintuple(
+            this.fourthElement,
+            this.secondElement,
+            this.thirdElement,
+            this.firstElement,
+            this.fifthElement
+        )
+
     companion object Builder {
 
         fun <FIRST_ELEMENT : Any, SECOND_ELEMENT : Any, THIRD_ELEMENT : Any, FOURTH_ELEMENT : Any, FIFTH_ELEMENT : Any> of(
--- library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/QuintupleTest.kt
+++ library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/QuintupleTest.kt
@@ -275,4 +275,31 @@ class QuintupleTest {
             assertEquals(fifthValue, result.fifthElement)
         }
     }
+
+    @Test
+    fun `switch first and fourth elements must return a quintuple with switched initial values`() {
+        val firstValue = Uuid.random().toString()
+        val secondValue = Uuid.random().toString()
+        val thirdValue = Uuid.random().toString()
+        val fourthValue = Uuid.random().toString()
+        val fifthValue = Uuid.random().toString()
+        val result =
+            Quintuple
+                .of(
+                    firstValue,
+                    secondValue,
+                    thirdValue,
+                    fourthValue,
+                    fifthValue
+                )
+                .switchFirstAndFourthElements()
+
+        assertSoftly {
+            assertEquals(fourthValue, result.firstElement)
+            assertEquals(secondValue, result.secondElement)
+            assertEquals(thirdValue, result.thirdElement)
+            assertEquals(firstValue, result.fourthElement)
+            assertEquals(fifthValue, result.fifthElement)
+        }
+    }
 }
