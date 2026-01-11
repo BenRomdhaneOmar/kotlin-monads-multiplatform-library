a0242e91e816cbf972c7600237cef6fbfa962510
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-11 13:04:18 +0100

    implement switch third and fifth function for quintuple tuple

--- library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/Quintuple.kt
+++ library/src/commonMain/kotlin/com/benromdhane/omar/offroadsoft/monad/tuple/Quintuple.kt
@@ -136,6 +136,15 @@ data class Quintuple<FIRST_ELEMENT : Any, SECOND_ELEMENT : Any, THIRD_ELEMENT : 
             this.fifthElement
         )
 
+    fun switchThirdAndFifthElements() =
+        Quintuple(
+            this.firstElement,
+            this.secondElement,
+            this.fifthElement,
+            this.fourthElement,
+            this.thirdElement
+        )
+
     companion object Builder {
 
         fun <FIRST_ELEMENT : Any, SECOND_ELEMENT : Any, THIRD_ELEMENT : Any, FOURTH_ELEMENT : Any, FIFTH_ELEMENT : Any> of(
