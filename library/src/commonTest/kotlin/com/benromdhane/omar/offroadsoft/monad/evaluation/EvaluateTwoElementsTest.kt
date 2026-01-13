31da5726b6e12bfe2e3bb0c8c5036adc186f3730
Omar BEN ROMDHANE <benromdhaneomar@gmail.com>
2026-01-13 23:31:32 +0100

    test evaluate two elements

--- library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/evaluation/EvaluateTwoElementsTest.kt
+++ library/src/commonTest/kotlin/com/benromdhane/omar/offroadsoft/monad/evaluation/EvaluateTwoElementsTest.kt
@@ -3,6 +3,7 @@ package com.benromdhane.omar.offroadsoft.monad.evaluation
 import io.kotest.assertions.assertSoftly
 import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
@@ -31,25 +32,149 @@ class EvaluateTwoElementsTest {
         }
     }
 
-//    multiple valid for first and one valid for second
-//    one valid for first and multiple valid for second
-//    multiple valid for first and multiple valid for second
-//    one invalid first and one invalid second
-//    multiple invalid first and one invalid second
-//    one invalid first and multiple invalid second
-//    multiple invalid first and multiple invalid second
-//    one valid first and one invalid second
-//    one invalid first and one valid second
-//    multiple valid first and one invalid second
-//    multiple valid first and multiple invalid second
-//    one valid first and multiple invalid second
-//    multiple invalid first and one valid second
-//    multiple invalid first and multiple valid second
-//    one invalid first and multiple valid second
-//    multiple first including one invalid and one second valid
-//    one first valid and multiple second including one invalid
-//    multiple first including one invalid and multiple second valid
-//    multiple first valid and multiple second including one invalid
-//    multiple first including one invalid and multiple second including one invalid
-//    multiple first including one invalid  and multiple second including one invalid
+    @Test
+    fun `evaluate must return success success or multiple errors if multiple valid evaluation for first element and multiple valid evaluation for second was provided`() {
+        val firstElementToEvaluate = Uuid.random().toString()
+        val secondElementToEvaluate = Random.nextInt()
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(firstElementToEvaluate)
+                .addEvaluation({ Exception(Uuid.random().toString()) }) { true }
+                .addEvaluation({ Exception(Uuid.random().toString()) }) { true }
+                .second(secondElementToEvaluate)
+                .addEvaluation({ Exception(Uuid.random().toString()) }) { true }
+                .addEvaluation({ Exception(Uuid.random().toString()) }) { true }
+                .evaluate()
+                .toMaybeSuccess()
+                .orNull()!!
+
+        assertSoftly {
+            assertEquals(firstElementToEvaluate, result.firstElement)
+            assertEquals(secondElementToEvaluate, result.secondElement)
+        }
+    }
+
+    @Test
+    fun `evaluate must return failure success or multiple errors if one invalid evaluation for first element and one invalid evaluation for second was provided`() {
+        val firstElementEvaluateError = Exception(Uuid.random().toString())
+        val secondElementEvaluateError = Exception(Uuid.random().toString())
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(Uuid.random().toString())
+                .addEvaluation({ firstElementEvaluateError }) { false }
+                .second(Random.nextInt())
+                .addEvaluation({ secondElementEvaluateError }) { false }
+                .evaluate()
+                .toErrors()
+
+        assertSoftly {
+            assertEquals(2, result.size)
+            assertContains(result, firstElementEvaluateError)
+            assertContains(result, secondElementEvaluateError)
+        }
+    }
+
+    @Test
+    fun `evaluate must return failure success or multiple errors if multiple invalid evaluation for first element and multiple invalid evaluation for second was provided`() {
+        val firstElementEvaluateError1 = Exception(Uuid.random().toString())
+        val firstElementEvaluateError2 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError1 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError2 = Exception(Uuid.random().toString())
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(Uuid.random().toString())
+                .addEvaluation({ firstElementEvaluateError1 }) { false }
+                .addEvaluation({ firstElementEvaluateError2 }) { false }
+                .second(Random.nextInt())
+                .addEvaluation({ secondElementEvaluateError1 }) { false }
+                .addEvaluation({ secondElementEvaluateError2 }) { false }
+                .evaluate()
+                .toErrors()
+
+        assertSoftly {
+            assertEquals(4, result.size)
+            assertContains(result, firstElementEvaluateError1)
+            assertContains(result, firstElementEvaluateError2)
+            assertContains(result, secondElementEvaluateError1)
+            assertContains(result, secondElementEvaluateError2)
+        }
+    }
+
+    @Test
+    fun `evaluate must return failure success or multiple errors if multiple valid evaluation for first element and multiple invalid evaluation for second was provided`() {
+        val firstElementEvaluateError1 = Exception(Uuid.random().toString())
+        val firstElementEvaluateError2 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError1 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError2 = Exception(Uuid.random().toString())
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(Uuid.random().toString())
+                .addEvaluation({ firstElementEvaluateError1 }) { true }
+                .addEvaluation({ firstElementEvaluateError2 }) { true }
+                .second(Random.nextInt())
+                .addEvaluation({ secondElementEvaluateError1 }) { false }
+                .addEvaluation({ secondElementEvaluateError2 }) { false }
+                .evaluate()
+                .toErrors()
+
+        assertSoftly {
+            assertEquals(2, result.size)
+            assertContains(result, secondElementEvaluateError1)
+            assertContains(result, secondElementEvaluateError2)
+        }
+    }
+
+    @Test
+    fun `evaluate must return failure success or multiple errors if multiple invalid evaluation for first element and multiple valid evaluation for second was provided`() {
+        val firstElementEvaluateError1 = Exception(Uuid.random().toString())
+        val firstElementEvaluateError2 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError1 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError2 = Exception(Uuid.random().toString())
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(Uuid.random().toString())
+                .addEvaluation({ firstElementEvaluateError1 }) { false }
+                .addEvaluation({ firstElementEvaluateError2 }) { false }
+                .second(Random.nextInt())
+                .addEvaluation({ secondElementEvaluateError1 }) { true }
+                .addEvaluation({ secondElementEvaluateError2 }) { true }
+                .evaluate()
+                .toErrors()
+
+        assertSoftly {
+            assertEquals(2, result.size)
+            assertContains(result, firstElementEvaluateError1)
+            assertContains(result, firstElementEvaluateError2)
+        }
+    }
+
+    @Test
+    fun `evaluate must return failure success or multiple errors if one of multiple invalid evaluation for first element and one of multiple valid evaluation for second was provided`() {
+        val firstElementEvaluateError1 = Exception(Uuid.random().toString())
+        val firstElementEvaluateError2 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError1 = Exception(Uuid.random().toString())
+        val secondElementEvaluateError2 = Exception(Uuid.random().toString())
+        val result =
+            EvaluateElements
+                .Two
+                .first<_, Throwable>(Uuid.random().toString())
+                .addEvaluation({ firstElementEvaluateError1 }) { false }
+                .addEvaluation({ firstElementEvaluateError2 }) { true }
+                .second(Random.nextInt())
+                .addEvaluation({ secondElementEvaluateError1 }) { false }
+                .addEvaluation({ secondElementEvaluateError2 }) { true }
+                .evaluate()
+                .toErrors()
+
+        assertSoftly {
+            assertEquals(2, result.size)
+            assertContains(result, firstElementEvaluateError1)
+            assertContains(result, secondElementEvaluateError1)
+        }
+    }
 }
